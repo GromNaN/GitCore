0866301550d2bbb006f6b2a345f91503c23980be
Jérôme Tamarelle <jerome@tamarelle.net>
2011-03-25 22:43:11 +0100

    Reorganizing

--- Ressources/config/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-// Set to true if you want to see all git commands and outputs
-define('GIT_DEBUG', true);
-
-// The git executable
-// define('GIT_EXEC', '/usr/bin/git');             // Linux
-// define('GIT_EXEC', 'command git');              // Mac
-// define('GIT_EXEC', 'C:\Program Files\Git\bin'); // Windows
